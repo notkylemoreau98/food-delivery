5cfd282e8fadf6c833ccfbcf98e2e54600fee486
Kyle Moreau <realkylemoreau@gmail.com>
2024-07-07 20:58:33 -0700

    Admin controller logic

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -3,6 +3,14 @@ import { CreateVendorInput } from '../dto';
 import { Vendor } from '../models';
 import { GeneratePassword, GenerateSalt } from '../utils';
 
+export const FindVendor = async (id: string | undefined, email?: string) => {
+  if (email) {
+    return await Vendor.findOne({ email: email });
+  } else {
+    return await Vendor.findById(id);
+  }
+};
+
 export const CreateVendor = async (
   req: Request,
   res: Response,
@@ -19,7 +27,7 @@ export const CreateVendor = async (
     phone,
   } = <CreateVendorInput>req.body;
 
-  const existingVendor = await Vendor.findOne({ email: email });
+  const existingVendor = await FindVendor('', email);
 
   if (existingVendor !== null) {
     return res.json({ message: 'A vendor already exists with this email ID' });
@@ -50,10 +58,28 @@ export const GetVendors = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  const vendors = await Vendor.find();
+
+  if (vendors !== null && vendors.length > 0) {
+    return res.json(vendors);
+  } else {
+    return res.json({ message: 'No Vendors Found' });
+  }
+};
 
 export const GetVendorByID = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  const vendorId = req.params.id;
+
+  const vendor = await FindVendor(vendorId);
+
+  if (vendor !== null) {
+    return res.json(vendor);
+  } else {
+    return res.json({ message: 'Vendor not found ' });
+  }
+};
